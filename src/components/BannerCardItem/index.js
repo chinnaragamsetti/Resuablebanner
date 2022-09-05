22b120dde8b60696f067ea4a03e5a5591a159902
rajkumar <ragamsettirajkumar@gmail.com>
2022-09-05 15:48:20 +0000

    resuable

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,13 @@
+// Write your code here.
+import './index.css'
+
+const userProfile=(props)=>{
+    const {headerText,description,className}=props;
+    <li className={`{cardItem}`className}>
+        <h1 className="h">{headerText}</h1>
+        <p className="p">{description}</p>
+        <button className="butt">showmore</button>
+    </li>
+}
+
+export default userProfile
